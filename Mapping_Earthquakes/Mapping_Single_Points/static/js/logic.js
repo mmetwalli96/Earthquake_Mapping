09b05fb8ac1dbf00bc536b074c26fa7686a957e4
Mohamed Metwalli <m.metwalli@outlook.com>
2021-09-22 05:42:43 -0400

    update

--- Mapping_Earthquakes/Mapping_Single_Points/static/js/logic.js
+++ Mapping_Earthquakes/Mapping_Single_Points/static/js/logic.js
@@ -1,14 +1,24 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
-
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
-//  Add a marker to the map for Los Angeles, California.
-let marker = L.circleMarker([34.0522, -118.2437], { 
-    rafius: 300,
-    color: "black",
-    fillColor: '#ffffa1'
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {radius: city.population/100000}).bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>").addTo(map);
+});
+
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "red"
 }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
